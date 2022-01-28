7ee4fc39a1a988bad8acb1188586d7a1242b31e0
Busiwa24 <busiwaliuma@gmail.com>
2022-01-28 12:45:23 +0200

    Changes to files to create counters

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+
+  def update_post_counter
+    users.posts_counter += 1
+  end
+
+  def recent_comments
+    comments.last(5)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :posts
+  has_many :likes, through: :posts
+  has_many :comments, through: :posts
+  def last_recent_posts
+    posts.last(3)
+  end
+end
