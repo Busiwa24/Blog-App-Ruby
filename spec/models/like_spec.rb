c691d0999538c983ff87c9bdcb4a1f2db6338412
Busiwa24 <busiwaliuma@gmail.com>
2022-01-28 23:16:28 +0200

    changes to spec model fles

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  describe 'Comment Likes Validations' do
-    subject { Like.new(author_id: 1, post_id: 1) }
+  subject { Like.new(created_at: Time.now, updated_at: Time.now, user_id: 1, post_id: 1) }
+  before { subject.save }
 
-    before { subject.save }
+  it 'User ID should be a number' do
+    subject.user_id = 'pizza'
+    expect(subject).to_not be_valid
+  end
 
-    it 'checks if author id is a number' do
-      subject.author_id = '45e%'
-      expect(subject).to_not be_valid
-    end
-
-    it 'checks if post id is a number' do
-      subject.post_id = 'look'
-      expect(subject).to_not be_valid
-    end
+  it 'Post ID should be a number' do
+    subject.post_id = 'pasta'
+    expect(subject).to_not be_valid
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,47 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  describe 'Post Model Validations' do
-    user = User.create(name: 'Dammy', bio: 'Dammy is so cool!')
-    subject do
-      Post.new(title: 'Bootstrap vs Tailwind', text: 'Let\'s  talk about Bootstrap and Tailwind', author_id: user)
-    end
+RSpec.describe Like, type: :model do
+  subject { Like.new(created_at: Time.now, updated_at: Time.now, user_id: 1, post_id: 1) }
+  before { subject.save }
 
-    before { subject.save }
+  it 'User ID should be a number' do
+    subject.user_id = 'pizza'
+    expect(subject).to_not be_valid
+  end
 
-    it 'checks if title is valid' do
-      subject.title = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'checks if title has more than 250 characters' do
-      expect(subject.title.length).to be <= 250
-    end
-
-    it 'validates that comments counter is an integer' do
-      subject.comments_counter = 'jdj'
-      expect(subject).to_not be_valid
-    end
-
-    it 'validates that comments counter is greater than or equal to 0' do
-      subject.comments_counter = -5.5
-      expect(subject).to_not be_valid
-    end
-
-    it 'validates that likes counter is an integer' do
-      subject.likes_counter = 'abc'
-      expect(subject).to_not be_valid
-    end
-
-    it 'validates that likes counter is greater than or equal to 0' do
-      subject.likes_counter = -1
-      expect(subject).to_not be_valid
-    end
-
-    describe 'should test methods in post model' do
-      it 'post should have five recent comments' do
-        expect(subject.recent_comments).to eq(subject.comments.last(5))
-      end
-    end
+  it 'Post ID should be a number' do
+    subject.post_id = 'pasta'
+    expect(subject).to_not be_valid
   end
 end
